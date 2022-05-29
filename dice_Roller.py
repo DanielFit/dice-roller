a0b6b0d31fef0f5c9065d7f98206c4e1aa1a5413
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-30 07:09:27 +1000

    Update dice_Roller.py

--- dice_Roller.py
+++ dice_Roller.py
@@ -1,5 +1,7 @@
 from random import randint
 
+""""This is a very simple piece of code to roll a number of dice of a certain size that could be used for games such as Monopoly, Yahtzee  ect"""
+
 #Dice pool
 d4 = 4
 d6 = 6
